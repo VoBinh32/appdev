438517fbe4f67e90c1eeb738a99ca2b15573de62
VoBinh32 <thanhbinh322000@gmail.com>
2019-04-03 16:26:56 +0300

    fix

--- screen.h
+++ screen.h
@@ -1,6 +1,6 @@
 // constant definitions
 #define UNICODE
-enum COLORS(BLACK=30, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE);
+enum COLORS{BLACK=30, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE};
 #define bg(c) (c+10)	//macro definition
 // function declarations
 void clearScreen(void);
