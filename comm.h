687d6666cb78a1a0bf71c697faa1d4b70ea0ed63
VoBinh32 <thanhbinh322000@gmail.com>
2019-04-17 16:20:41 +0300

    add fix sound

--- comm.h
+++ comm.h
@@ -1,6 +1,6 @@
 // constant definition
 
-#define  URL "http://www.cc.puv.fi/~e1800926/Newfolder/comm.php")
+#define  URL "http://www.cc.puv.fi/~e1800926/Newfolder/comm.php"
 
 // function declarations
 
